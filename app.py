1e4d7b1fc32a245acb1b2c5c6aae6b3f83fb95f9
lesfgoh <htho002@e.ntu.edu.sg>
2024-09-09 14:17:57 +0800

    Added a certifications page

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ def projects():
 def about():
     return render_template('about.html')
 
+@app.route('/certifications')
+def certifications():
+    return render_template('certifications.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
